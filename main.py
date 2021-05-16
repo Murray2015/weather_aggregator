311965ff1f8263fb468b9c945dd63179a0d72a6d
Murray2015 <murrayhoggett@gmail.com>
2021-05-16 17:05:01 +0100

    Added processing for Accuweather client

--- main.py
+++ main.py
@@ -280,7 +280,8 @@ class AccuWeatherClient(WeatherDataClient):
         self.params = {
             'apikey': ACCUWEATHER_API_KEY,
             'language': 'en-us',
-            'details': "true"
+            'details': "true",
+            'metric': 'true'
         }
 
     def get_location_code(self, lat, lon):
@@ -301,8 +302,29 @@ class AccuWeatherClient(WeatherDataClient):
         forecast_data = self.get_forecast(location_code=location_code)
         return forecast_data
 
-    def process_data(self):
-        pass
+    def process_data(self, data, lat, lon):
+        print(data)
+        processed_weather_data = []
+        place_name = None
+        for hourly in data:
+            processed_data = {
+                'lat': lat,
+                'lon': lon,
+                'place_name': place_name,
+                'date_time': datetime.fromtimestamp(hourly['EpochDateTime']),
+                'temperature_celcius': float(hourly['Temperature']['Value']),
+                'feels_like_temperature_celcius': float(hourly['RealFeelTemperature']['Value']),
+                'wind_speed_kph': float(hourly['Wind']['Speed']['Value']),
+                'wind_direction': hourly['Wind']['Direction']['Localized'],
+                'wind_gust_mph': float(hourly['WindGust']['Speed']['Value']),
+                'relative_humidity_percentage': float(hourly['RelativeHumidity']),
+                'visibility': open_weather_visibility_lookup(hourly['Visibility']['Value'] * 1000),
+                'uv_index': {'code': hourly['UVIndex'], 'description': uv_lookup_codes[str(hourly['UVIndex'])]},
+                'weather_type': accuweather_weather_code_lookup[hourly['WeatherIcon']],
+                'precipitation_probability_percentage': float(hourly['PrecipitationProbability']),
+            }
+            processed_weather_data.append(processed_data)
+        return processed_weather_data
 
     def get_forecast_postcode(self, country, postcode):
         lat, lon = self.geocode_postcode(country, postcode)
@@ -310,17 +332,21 @@ class AccuWeatherClient(WeatherDataClient):
         forecast_data = self.get_forecast(location_code=location_code)
         return forecast_data
 
-    def get_forecast_city_country(self):
-        pass
+    def get_forecast_city_country(self, city, country):
+        lat, lon = self.geocode_city_country(city, country)
+        location_code = self.get_location_code(lat, lon)
+        forecast_data = self.get_forecast(location_code=location_code)
+        processed_data = self.process_data(forecast_data, lat, lon)
+        return processed_data
 
 
 accuweather = AccuWeatherClient()
 # print('accuweather get_forecast_lat_lon', accuweather.get_forecast_lat_lon(
 #     lat=50.73862, lon=-2.90325))
-print('accuweather get_forecast_postcode',
-      accuweather.get_forecast_postcode('GB', 'b17 0hs'))
-# print('accuweather get_forecast_city_country',
-#       accuweather.get_forecast_city_country("Birmingham", "uk"))
+# print('accuweather get_forecast_postcode',
+#       accuweather.get_forecast_postcode('GB', 'b17 0hs'))
+print('accuweather get_forecast_city_country',
+      accuweather.get_forecast_city_country("Birmingham", "uk"))
 
 
 class TomorrowIOClient(WeatherDataClient):
